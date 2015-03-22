cc9f562f503cd0e52be7c3347fa4428c03089900
raven5887 <2babfbd2@opayq.com>
2015-03-22 22:09:40 +0000

    First commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -86,22 +86,41 @@ jQuery(function($) {
         });
     });
 
-    $('body').on('click', '.js-ajax-link, .pagination a, .post-tags a, .post-header a', function(e) {
+    $('body').on('tap', '.js-ajax-link, .pagination a, .post-tags a, .post-header a, .about-btn a', function(e) {
         e.preventDefault();
 
         if (loading === false) {
             var currentState = History.getState();
             var url = $(this).attr('href');
             var title = $(this).attr('title') || null;
-
-            $('.post-list .active').removeClass('active');
-            $(e.target).addClass('active');
+            
 
             // If the requested url is not the current states url push
             // the new state and make the ajax call.
-            if (url !== currentState.url.replace(/\/$/, "")) {
+            if (!(currentState.url.indexOf(url) > -1)) {
+            //if (url !== currentState.url.replace(/\/$/, "")) { // Old unreliable test from legacy code
                 loading = true;
 
+                $('.post-list .active').removeClass('active');
+                
+                var newActiveButton = $(e.target).closest('li');
+                newActiveButton.addClass('active');
+                
+                if (newActiveButton.hasClass('about-btn')){
+                    setActiveNavBarButton($("#about"));
+                }else if(newActiveButton.hasClass('tag-gallery')){
+                    setActiveNavBarButton($("#gallery"));
+                }else{ // Assume its a blog? D:
+                    setActiveNavBarButton($("#blog"));
+                }
+                
+                if(url.substring(0,8)=="/author/"){
+                    $('#site-comments').css('display','none');
+                }else{
+                    $('#site-comments').css('display','block');
+                    $(".fb-comments").attr('data-href', window.location.protocol + "//" + window.location.host + url);
+                    FB.XFBML.parse();  
+                }
                 // Check if we need to show the post index after we've
                 // loaded the new content
                 if ($(this).hasClass('js-show-index') || $(this).parent('.pagination').length > 0) {
@@ -111,7 +130,7 @@ jQuery(function($) {
                 NProgress.start();
 
                 History.pushState({}, title, url);
-            } else {
+            } /*else { // Legacy code
                 // Swap in the latest post or post index as needed
                 if ($(this).hasClass('js-show-index')) {
                     $('html, body').animate({'scrollTop': 0});
@@ -132,13 +151,19 @@ jQuery(function($) {
                         NProgress.done();
                     });
                 }
-            }
+            }*/
         }
     });
     
-    if ( location.pathname !== '/' ) {
-        $('#site-canvas > menu').load('/ #site-canvas > menu > li', function() {
+    if ( location.pathname !== '/' ) { // If not bare url
+        $('#site-canvas > menu').load('/ #site-canvas > menu.post-list > li', function() {//Load postbar from main page into gblog/gallery pages, ugly hack
             rigEverything();
+            var itemType = $("meta[name='item-type']").attr("content");
+            var itemUrl = $("meta[name='item-url']").attr("content");
+            if(itemUrl)
+                $("a[href='"+itemUrl+"']").parent().addClass("active");
+                
+            togglePostBarMode(null,itemType);
         });
     }else{
         rigEverything();
@@ -146,15 +171,15 @@ jQuery(function($) {
     
     function rigEverything(){
         moveImagesToGallery();
-        hookImageButtons();
+        togglePostBarMode();
         
         // Toggle Nav or Comments on Click
-        $('.toggle-nav').on('tap',function() {
-            toggleNav();
-        });
+        $('.toggle-nav').on('tap',toggleNav);
+        $('.toggle-comment').on('tap',toggleComment);
         
-        $('.toggle-comment').on('tap',function() {
-            toggleComment();
+        $('.nav-btn').on('tap',function(tap){
+            togglePostBarMode(tap);
+            toggleNav(true);
         });
         
         // Make sidebar images black and white
@@ -163,120 +188,185 @@ jQuery(function($) {
         });
         
         // Make sidebar scrollbar function properly    
-       resizePostBar(); //Init scrollbar
-        var scrollPane = $('.scroll-pane').jScrollPane(
-    		{
-                mouseWheelSpeed: 50
-    		}
-    	);
-    	
-        api = scrollPane.data('jsp');
-        $( window ).load(function() {
-            setTimeout(reinitialiseScrollBar, 100);
+        if (window.matchMedia('(min-width: 741px)').matches){ //If in desktop-mode
+            resizePostBar(); //Init scrollbar
+            /*var scrollPane = $('.scroll-pane').jScrollPane(
+        		{
+                    mouseWheelSpeed: 50
+        		}
+        	);
+        	
+            api = scrollPane.data('jsp');*/
+            $( window ).load(function() { //Doesnt fire
+                setTimeout(reinitialiseScrollBar, 100);
+                $(".scroll-pane").scroller({
+                	customClass: "advanced",
+                	handleSize: 40
+                });
+                for (var i = 1; i <= 10; i++) { 
+                    setTimeout(reinitialiseScrollBar, i*150);
+                }
+            });
+            /*(function() {
+                setTimeout(reinitialiseScrollBar, 100);
+                $(".scroll-pane").scroller({
+                	customClass: "advanced",
+                	trackMargin: 5,
+                	handleSize: 40
+                });
+                for (var i = 1; i <= 10; i++) { 
+                    setTimeout(reinitialiseScrollBar, 1*100);
+                }
+            })();*/
+            
+            var debounce;
+            $(window).on('resize', function(){ //re-init scrollbar on resizing
+                resizePostBar();
+                clearTimeout(debounce);
+                debounce = setTimeout(reinitialiseScrollBar, 500);
+            });
+        }
+    }
+    
+    /*========================================
+    =               FUNCTIONS               =
+    ========================================*/
+    function togglePostBarMode(tap, manualToggle){ //Setting the blog value to a boolean forces the mode
+        if((tap && $(tap.delegateTarget).attr('id')=='blog') || manualToggle == "blog"){
+            $(".post-list li.post:not(.tag-gallery)").show();
+            $(".post-list li.post.tag-gallery").hide();
+            setActiveNavBarButton($("#blog"));
+        }else if((tap && $(tap.delegateTarget).attr('id')=='about') || manualToggle == "about"){
+            setActiveNavBarButton($("#about"));
+        }else{
+            $(".post-list li.post:not(.tag-gallery)").hide();
+            $(".post-list li.post.tag-gallery").show();
+            setActiveNavBarButton($("#gallery"));
+        }
+        setTimeout(reinitialiseScrollBar, 100);
+    }
+    
+    function setActiveNavBarButton(btn){
+        $("#site-menu > menu").find(".active").removeClass("active");
+        btn.addClass("active");
+    }
+    
+    function toggleNav(turnOff) {
+        $('#wrapper').removeClass('show-comments');
+        
+        if ($('#wrapper').hasClass('show-nav')) {
+            $('#wrapper').removeClass('show-nav');
+        } else if(turnOff != null) {
+            $('#wrapper').addClass('show-nav');
+        }
+    }
+    
+    function toggleComment() {
+        
+        $('#wrapper').removeClass('show-nav');
+        
+        if ($('#wrapper').hasClass('show-comments')) {
+            $('#wrapper').removeClass('show-comments');
+        } else {
+            $('#wrapper').addClass('show-comments');
+        }
+    }
+    
+    function toggleDescNav() {
+        console.log("asdeasd");
+        var footer = $('.gallery-footer');
+        if (footer.hasClass('active')) {
+            footer.removeClass('active');
+        } else {
+            footer.addClass('active');
+        }
+    }
+        
+    function resizePostBar(){
+        var windo = $(this); //this = window
+        if ($('#site-canvas').width() > 740) {
+            $('#site-canvas').css('height', (windo.height()-62) + 'px');
+        }
+    }
+        
+    function setBlackAndWhite(image){
+        var bnbImage = image.clone();
+        
+        //bnbImage.attr('src', image.attr('src'));
+        bnbImage.attr('class', 'bnb-image');
+        bnbImage.insertAfter( image );
+        image.css('opacity',1);
+    }
+    
+    function reinitialiseScrollBar(){
+        if (window.matchMedia('(min-width: 741px)').matches){ //If in desktop-mode
+            //console.log("ASDASDASD");
+            //api.reinitialise();
+            //$('.post-list .jspPane').css('width','264px');
+            $('.scroll-pane').scroller("reset");
+        }
+    }
+    
+    function hookImageNavigationButtons(){
+        var gallery = $('section.gallery-content');
+        var items = gallery.children().length - 1;
+        if(gallery.length){
+            var menu = $('#image-menu');
+            var buttons = $('#image-menu .image-nav ');
+            var currentPosition = 0;
+            
+            buttons.on('tap',function(event){
+                var direction = 'left';
+                if (window.matchMedia('(min-width: 741px)').matches){ //If in desktop-mode
+                    direction = 'top';
+                }
+                if($(event.target).hasClass('previous')){
+                    currentPosition !== 0 ? (currentPosition += -1) : 0;
+                    currentPosition === 0 && menu.find('.previous').addClass('disabled');
+                    menu.find('.next').removeClass('disabled');
+                }else{
+                    currentPosition !== (items) ? (currentPosition += 1) : items;
+                    currentPosition === items && menu.find('.next').addClass('disabled');
+                    menu.find('.previous').removeClass('disabled');
+                }
+                gallery.css(direction, (currentPosition * -100) + '%');
+            });
+            
+        }
+    }
+    
+    function moveImagesToGallery(){
+        var post = $('#ajax-container  footer > div.gallery-description');
+        var images = post.find('img');
+        var gallery = $('#ajax-container section.gallery-content');
+        images.each(function(i, image){
+            var imageDiv = $(document.createElement('div'));
+            imageDiv.css({
+                backgroundSize: 'cover',
+                backgroundImage: 'url('+image.getAttribute('src')+')',
+                backgroundRepeat: 'no-repeat',
+            });
+            if(image.hasAttribute('alt') && image.getAttribute('alt').contains('focus')){
+                var alt = image.getAttribute('alt');
+                var n = alt.indexOf("focus");
+                var p = alt.indexOf("%",alt.indexOf("%",n)+1);
+                imageDiv.css(
+                    'background-position', alt.substring(n+6,p+1).replace('x',' ')
+                );
+            }
+            imageDiv.attr({
+                title: image.getAttribute('alt'),
+                class: 'image'
+            });
+            image.remove();
+            gallery.append(imageDiv);
         });
         
-        var debounce;
-        $(window).on('resize', function(){ //re-init scrollbar on resizing
-            resizePostBar();
-            clearTimeout(debounce);
-            debounce = setTimeout(reinitialiseScrollBar, 500);
-        });
+        hookImageNavigationButtons();
+        $('.description-nav-toggle').on('tap',toggleDescNav);
+        
     }
 });
 
     
     
-/*========================================
-=            CUSTOM FUNCTIONS            =
-========================================*/
-function toggleNav() {
-    
-    $('#wrapper').removeClass('show-comments');
-    
-    if ($('#wrapper').hasClass('show-nav')) {
-        $('#wrapper').removeClass('show-nav');
-    } else {
-        $('#wrapper').addClass('show-nav');
-    }
-}
-
-function toggleComment() {
-    
-    $('#wrapper').removeClass('show-nav');
-    
-    if ($('#wrapper').hasClass('show-comments')) {
-        $('#wrapper').removeClass('show-comments');
-    } else {
-        $('#wrapper').addClass('show-comments');
-    }
-}
-    
-function resizePostBar(){
-    var windo = $(this); //this = window
-    if ($('#site-canvas').width() >= 501) {
-        $('#site-canvas').css('height', (windo.height()-62) + 'px');
-    }
-}
-    
-function setBlackAndWhite(image){
-    var bnbImage = image.clone();
-    
-    //bnbImage.attr('src', image.attr('src'));
-    bnbImage.attr('class', 'bnb-image');
-    bnbImage.insertAfter( image );
-    image.css('opacity',1);
-}
-
-function reinitialiseScrollBar(){
-    api.reinitialise();
-    $('.post-list .jspPane').css('width','264px');
-}
-
-function hookImageButtons(){
-    var gallery = $('section.gallery-content');
-    var items = gallery.children().length - 1;
-    if(gallery.length){
-        var buttons = $('#image-menu > div i');
-        var currentPosition = 0;
-        
-        buttons.on('tap',function(event){
-            console.log(items);
-            if($(event.target).hasClass('previous')){
-                currentPosition !== 0 ? (currentPosition += -1) : 0;
-            }else{
-                currentPosition !== (items) ? (currentPosition += 1) : (items);
-            }
-            gallery.css('top', (currentPosition * -100) + '%');
-        });
-        
-    }
-}
-
-function moveImagesToGallery(){
-    var post = $('#ajax-container  footer > div.gallery-description');
-    var images = post.find('img');
-    var gallery = $('#ajax-container section.gallery-content');
-    images.each(function(i, image){
-        var imageDiv = $(document.createElement('div'));
-        imageDiv.css({
-            backgroundSize: 'cover',
-            backgroundImage: 'url('+image.getAttribute('src')+')',
-            backgroundRepeat: 'no-repeat',
-        });
-        if(image.hasAttribute('alt') && image.getAttribute('alt').contains('focus')){
-            var alt = image.getAttribute('alt');
-            var n = alt.indexOf("focus");
-            var p = alt.indexOf("%",alt.indexOf("%",n)+1);
-            imageDiv.css(
-                'background-position', alt.substring(n+6,p+1).replace('x',' ')
-            );
-        }
-        imageDiv.attr({
-            title: image.getAttribute('alt'),
-            class: 'image'
-        });
-        image.remove();
-        gallery.append(imageDiv);
-    });
-    
-}
